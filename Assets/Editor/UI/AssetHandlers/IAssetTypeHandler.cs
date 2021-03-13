a3afde523f4af88b337fbc341698cfa04831db13
LinqToException <47725963+LinqToException@users.noreply.github.com>
2021-03-13 06:50:11 +0100

    I forgot to add the code, didn't I

---
+++ Assets/Editor/UI/AssetHandlers/IAssetTypeHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace ParkitectAssetEditor.UI.AssetHandlers
+{
+    public interface IAssetTypeHandler
+    {
+        /// <summary>
+        /// Called by <see cref="AssetEditorWindow.DrawAssetDetailSection"/>
+        /// and used to draw the detail asset settings.
+        /// </summary>
+        /// <param name="selectedAsset"></param>
+        void DrawDetailsSection(Asset selectedAsset);
+    }
+}
